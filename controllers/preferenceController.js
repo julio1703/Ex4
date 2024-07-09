84d0a38b1d76c6b30f5f70f75511677f1d4de089
julio1703 <89707649+julio1703@users.noreply.github.com>
2024-07-09 13:54:32 +0300

    added function getAllPreferences

--- controllers/preferenceController.js
+++ controllers/preferenceController.js
@@ -1,5 +1,18 @@
 const {dbConnection} = require("../db_connection");
 
-const preferencesController = {};
+const preferencesController = {
+    async getAllPreferences(req, res) {
+        const connection = await dbConnection.createConnection();
+
+        try {
+            const [rows] = connection.execute("SELECT * FROM tbl_27_preferences");
+            res.status(200).json(rows);
+            await connection.end();
+        } catch (error) {
+            res.status(500).json({error: error.message});
+            await connection.end();
+        }
+    }
+};
 
 module.exports = { preferencesController };
